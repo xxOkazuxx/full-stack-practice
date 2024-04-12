2703432528efa69f67a7f97075a92e18a12ffebd
Kazuki Ochiai <kazu910739@gmail.com>
2024-04-12 21:53:55 +0900

    tailwindcssを導入

--- full-stack-frontend/src/App.tsx
+++ full-stack-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1>Full Stack Application is here!!</h1>
+      <h1 className="text-4xl font-bold underline">Full Stack Application is here!!</h1>
     </>
   )
 }
