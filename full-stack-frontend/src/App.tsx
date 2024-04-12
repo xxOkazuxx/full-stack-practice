13d0bdda0e8a2699f0f916be50f97ea455ad280e
Kazuki Ochiai <kazu910739@gmail.com>
2024-04-12 22:09:33 +0900

    DaisyUIを導入

--- full-stack-frontend/src/App.tsx
+++ full-stack-frontend/src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <>
       <h1 className="text-4xl font-bold underline">Full Stack Application is here!!</h1>
+      <button className="btn btn-primary">Button</button>
     </>
   )
 }
