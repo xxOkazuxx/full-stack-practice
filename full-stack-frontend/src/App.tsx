1c36eb1566d12075c41337db860fdd91b147cd8e
xxOkazuxx <87918360+xxOkazuxx@users.noreply.github.com>
2024-04-12 23:44:18 +0900

    TailwindcssとDaisyUIの導入 (#1)
    * tailwindcssを導入
    
    * DaisyUIを導入
    
    * DaisyUIのテーマを変更

--- full-stack-frontend/src/App.tsx
+++ full-stack-frontend/src/App.tsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
     <>
-      <h1>Full Stack Application is here!!</h1>
+      <h1 className="text-4xl font-bold underline">Full Stack Application is here!!</h1>
+      <button className="btn btn-primary">Button</button>
     </>
   )
 }
