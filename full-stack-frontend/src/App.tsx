a0d406cef1120d758f12c7bb9bdd54bbfb72c7ef
Kazuki Ochiai <kazu910739@gmail.com>
2024-04-29 22:19:29 +0900

    ドロップダウンをお試し

--- full-stack-frontend/src/App.tsx
+++ full-stack-frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
-import Form from './components/Form';
+import { useState } from "react";
+import Form from "./components/Form";
+import DropDown from "./components/DropDown";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='flex justify-center m-10'>
-        <h1 className="text-4xl font-bold underline">Full Stack Application is here!!</h1>
-      </div >
-      <div className='flex justify-center'>
-        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Button: {count}</button>
+      <div className="flex justify-center m-10">
+        <h1 className="text-4xl font-bold underline">
+          Full Stack Application is here!!
+        </h1>
+      </div>
+      <div className="flex justify-center">
+        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+          Button: {count}
+        </button>
       </div>
       <Form />
+      <DropDown />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
